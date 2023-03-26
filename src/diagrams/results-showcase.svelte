ba224b3bd526901f467b7d808e202a39d2f9eb68
luigi1982 <61950954+luigi1982@users.noreply.github.com>
2023-03-26 22:14:41 +0200

    meine version

---
+++ src/diagrams/results-showcase.svelte
@@ -0,0 +1,30 @@
+<script>
+  const len = 3;
+  let count = 0;
+  let hasInteracted = false;
+  let cgitscons = [10,20,50,100];
+  let envcons = 2;
+  
+  // environment 1:LunarLander, 2:CartPole
+  
+  //function inputCartM(x) {
+  //	document.getElementById("content").innerHTML='<object type="text/html" data="html/CartM.html" ></object>';
+  //}
+  
+
+</script>
+
+<div style="display: flex; flex-direction: column; align-items: center; padding-top: 1em">
+  <div style="text-align:center;">
+    <input type="checkbox" id="CartM" name="CartM" value="CartM" on:input={inputCartM}>
+    <label for="CartM"> CartPole, stay mid</label>
+    <input type="checkbox" id="CartL" name="CartL" value="CartL" on:input={inputCartL}>
+    <label for="CartL"> CartPole, go left</label>
+    <input type="checkbox" id="LunarS" name="LunarS" value="LunarS" on:input={inputLunarS}>
+    <label for="LunarS"> LunarLander, speed constraint</label>
+    <input type="checkbox" id="LunarA" name="LunarA" value="LunarA" on:input={inputLunarA}>
+    <label for="LunarA"> LunarLander, angle constraint</label>
+  </div>
+  <div id="content"> hello
+  </div>
+</div>

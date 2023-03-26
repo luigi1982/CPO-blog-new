c46925c5e62f5c48814de861704fb70f252ae30c
dieforice <dieforice@gmail.com>
2023-03-26 21:40:55 +0200

    #1

---
+++ src/diagrams/comp_tr_reward.svelte
@@ -0,0 +1,31 @@
+<script>
+  const len = 3;
+  let count = 0;
+  let hasInteracted = false;
+  let envcons = "CartPole";
+  
+  // environment 1:LunarLander, 2:CartPole
+  
+  function inputLcons(x) {
+  	document.getElementById("LunarBoxcons").checked = false;
+  	envcons = "LunarLander";
+  }
+  
+  function inputCcons(x) {
+  	document.getElementById("CartBoxcons").checked = false;
+  	envcons= "CartPole";
+  }
+</script>
+
+<div style="display: flex; flex-direction: column; align-items: center; padding-top: 1em">
+  <div style="text-align:center;">
+    <input type="checkbox" id="CartBoxcons" name="CartBoxcons" value="Cart" on:input={inputCcons}>
+    <label for="CartBoxcons"> CartPole</label>
+    <input type="checkbox" id="LunarBoxcons" name="LunarBoxcons" value="Lunar" on:input={inputLcons}>
+    <label for="LunarBoxcons"> LunarLander</label>
+  </div>
+  <img
+    src="images/{envcons}_tr_reward.svg"
+    alt="Interactive plot comparing behaviour of the constraint value for different trust region size"
+  />
+</div>
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import Example from "./diagrams/svelte-example.svelte";
 import CGits from "./diagrams/different-cgits.svelte";
 import CGitsRes from "./diagrams/different-cgits-residual.svelte";
 import comp10vs100 from "./diagrams/comp10vs100cg.svelte";
+import compTrReward from "./diagrams/comp_tr_reward.svelte";
+import compTrConstraint from "./diagrams/comp_tr_constraint.svelte";
 
 // lazily initialize any diagram below the fold. E.G:
 const exampleTag = document.getElementById("svelte-example-dfigure");
@@ -35,3 +37,18 @@ cgits10vs100Tag.addEventListener("ready", () => {
 	const target = cgits10vs100Tag.querySelector("#constraint-visualization-target");
 	cgits10vs100 = new comp10vs100({ target });
 });
+
+const trRewardTag = document.getElementById("constraint-visualization-trust-region-reward");
+let trReward;
+trRewardTag.addEventListener("ready", () => {
+	const target = trRewardTag.querySelector("#constraint-visualization-trust-region-reward-target");
+	trReward = new compTrReward({ target });
+});
+
+const trConstraintTag = document.getElementById("constraint-visualization-trust-region-constraint");
+let trConstraint;
+trConstraintTag.addEventListener("ready", () => {
+	const target = trConstraintTag.querySelector("#constraint-visualization-trust-region-constraint-target");
+	trConstraint = new compTrConstraint({ target });
+});
+
